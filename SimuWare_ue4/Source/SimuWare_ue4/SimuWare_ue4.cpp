fec6b1807e581c5f86ba16b5aa9183d1a08171c2
Dhruv Kukreja <94745898+RadioactiveDK@users.noreply.github.com>
2024-02-09 12:12:36 +0530

    Initial build

---
+++ SimuWare_ue4/Source/SimuWare_ue4/SimuWare_ue4.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimuWare_ue4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimuWare_ue4, "SimuWare_ue4" );
+ 
---
+++ SimuWare_ue4/Source/SimuWare_ue4/SimuWare_ue4GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimuWare_ue4GameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASimuWare_ue4GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASimuWare_ue4GameMode();
+};
+
+
+
